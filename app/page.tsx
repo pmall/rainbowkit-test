60102f7b44d9dd9cbedc9009e2460acd18916394
pmall <pierre.mallinjoud@gmail.com>
2023-01-17 17:05:49 +0100

    add send transaction form

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,106 @@
-import { Button } from '../components/Button'
+'use client';
+
+import { useState } from 'react';
+import { useDebounce } from 'use-debounce';
+import { useConnectModal } from '@rainbow-me/rainbowkit';
+import { useAccount, usePrepareSendTransaction, useSendTransaction, useWaitForTransaction } from 'wagmi';
+import { utils, BigNumber } from 'ethers';
+
+import { Button } from '../components/Button';
 
 export default function Home() {
+  const { isConnected } = useAccount()
+  const { openConnectModal } = useConnectModal()
+
+  const [to, setTo] = useState<string>('')
+  const [debouncedTo] = useDebounce(to, 500)
+
+  const [amount, setAmount] = useState<string>('')
+  const [debouncedAmount] = useDebounce(amount, 500)
+
+  const parsedAmount = debouncedAmount ? utils.parseEther(debouncedAmount) : BigNumber.from(0)
+
+  const prepare = usePrepareSendTransaction({
+    request: {
+      to: debouncedTo,
+      value: parsedAmount,
+    },
+  })
+
+  const tx = useSendTransaction(prepare.config)
+
+  const result = useWaitForTransaction({
+    hash: tx.data?.hash,
+  })
+
+  const preparing = prepare.isLoading
+
+  const sending = tx.isLoading || result.isLoading
+
+  const valid = true
+    && to === debouncedTo
+    && amount === debouncedAmount
+    && debouncedTo.trim().length > 0
+    && debouncedAmount.trim().length > 0
+    && parsedAmount.gt(BigNumber.from(0))
+    && !prepare.isError
+
+  const disabled = preparing || sending || !valid
+
+  const isError = isConnected && (prepare.isError || tx.isError)
+
+  const isSuccess = isConnected && result.isSuccess && !isError
+
+  const error = prepare.error || tx.error
+
   return (
     <main>
       <h1 className="mb-4 text-3xl font-bold underline">
         Send transaction
       </h1>
-      <form>
+      <form onSubmit={e => { e.preventDefault(); tx.sendTransaction?.() }}>
         <div className="flex flex-col space-y-4 w-80 mx-auto">
           <label className="block">
             <span className="text-gray-700">Recipient</span>
-            <input type="text" placeholder="recipient" className="block rounded w-full" />
+            <input
+              type="text"
+              placeholder="recipient"
+              className="block rounded w-full"
+              onChange={e => setTo(e.target.value)}
+            />
           </label>
           <label className="block">
             <span className="text-gray-700">Amount</span>
-            <input type="number" placeholder="amount" className="block rounded w-full" />
+            <input
+              type="number"
+              step="any"
+              placeholder="amount"
+              className="block rounded w-full"
+              onChange={e => setAmount(e.target.value)}
+            />
           </label>
-          <Button type="submit">
-            Send
-          </Button>
+          {isConnected && (
+            <Button type="submit" disabled={disabled}>
+              {sending ? 'Sending...' : 'Send'}
+            </Button>
+          )}
+          {!isConnected && (
+            <Button onClick={() => openConnectModal?.()}>
+              Plase connect your wallet
+            </Button>
+          )}
+          {isSuccess && (
+            <p className="text-green-600">
+              <a
+                className="block"
+                href={`https://etherscan.io/tx/${tx.data?.hash}`}
+                target="_blank"
+              >Successfully sent {amount} ether to {to}</a>
+            </p>
+          )}
+          {isError && (
+            <pre className="text-red-500">{JSON.stringify(error, null, 2)}</pre>
+          )}
         </div>
       </form>
     </main>
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,17 +1,15 @@
-export function Button({ type = "button", children }: {
-  type: "button" | "submit" | "reset" | undefined,
-  children: React.ReactNode,
+export function Button({ type = "button", disabled = false, onClick = () => { }, children = null }: {
+  type?: "button" | "submit" | "reset" | undefined,
+  disabled?: boolean,
+  onClick?: () => void
+  children?: React.ReactNode,
 }) {
+  const classes = disabled
+    ? 'inline-block px-6 py-2.5 bg-blue-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md focus:outline-none focus:ring-0 transition duration-150 ease-in-out pointer-events-none opacity-60'
+    : 'inline-block px-6 py-2.5 bg-blue-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-600 hover:shadow-lg focus:bg-blue-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-700 active:shadow-lg transition duration-150 ease-in-out'
+
   return (
-    <button
-      type="submit"
-      data-mdb-ripple="true"
-      data-mdb-ripple-color="light"
-      className="
-        inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md
-        hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800
-        active:shadow-lg transition duration-150 ease-in-out
-      ">
+    <button type={type} disabled={disabled} className={classes} onClick={onClick}>
       {children}
     </button>
   )
