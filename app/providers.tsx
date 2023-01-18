a3d0f4d94783e7ed3bb978ea837ace8b972dfe47
pmall <pierre.mallinjoud@gmail.com>
2023-01-18 17:23:33 +0100

    add alchemy provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,9 +4,13 @@ import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
 import { goerli } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
-const { chains, provider } = configureChains([goerli], [publicProvider()])
+const { chains, provider } = configureChains([goerli], [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY ?? '' }),
+    publicProvider(),
+])
 
 const { connectors } = getDefaultWallets({ appName: 'My test app', chains })
 
